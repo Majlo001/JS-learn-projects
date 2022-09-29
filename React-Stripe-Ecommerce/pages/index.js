abb2e5ffb104d13761ef5bd46d8d95e017140b97
Majlo001 <45098860+Majlo001@users.noreply.github.com>
2022-09-29 10:37:25 +0200

    React-Stripe-Ecommerce

---
+++ React-Stripe-Ecommerce/pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h2>Laptops</h2>
+        <div className="py-4">
+          <div className="w-64">
+            <div className="p-5 rounded-xl bg-blue-300">
+              <img src='products/laptops/samsung.png' alt='' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
