e4aceffa26cdb49768e7d6ce8bdcfffebb39441c
Majlo001 <szmilosz01@gmail.com>
2022-11-04 19:58:17 +0100

    react-little-crm 0.2

--- react-little-crm/src/components/TicketCard.js
+++ react-little-crm/src/components/TicketCard.js
@@ -1,6 +1,23 @@
-const TicketCard = () => {
+import { Link } from 'react-router-dom';
+import AvatarDisplay from './AvatarDisplay';
+import StatusDisplay from './StatusDisplay';
+import PriorityDisplay from './PriorityDisplay';
+import ProgressDisplay from './ProgressDisplay';
+import DeleteBlock from './DeleteBlock';
+
+const TicketCard = ({color, ticket}) => {
     return (
-        <div>TicketCard</div>
+        <div className="ticket-card">
+            <div className="ticket-card__color"></div>
+            <Link to={`/ticket/${ticket.documentId}`} id="link">
+                <h3>{ticket.title}</h3>
+                <AvatarDisplay/>
+                <StatusDisplay/>
+                <PriorityDisplay/>
+                <ProgressDisplay/>
+            </Link>
+            <DeleteBlock/>
+        </div>
     )
 }
 
--- react-little-crm/src/pages/Dashboard.js
+++ react-little-crm/src/pages/Dashboard.js
@@ -1,12 +1,66 @@
 import TicketCard from '../components/TicketCard';
 
 const Dashboard = () => {
+
+    const tickets = [
+        {
+            category: 'Q1 2023',
+            color: 'red',
+            title: 'NFT will break the world.',
+            avatar: '',
+            status: 'done',
+            priority: 5,
+            progress: 40,
+            description: 'Description here.',
+            timestamp: '2023-01-23T09:30:00+0000'
+        },
+        {
+            category: 'Q3 2023',
+            color: 'blue',
+            title: 'NFT Blue',
+            avatar: '',
+            status: 'done',
+            priority: 2,
+            progress: 55,
+            description: 'Description here.',
+            timestamp: '2023-08-23T09:30:00+0000'
+        },
+        {
+            category: 'Q1 2023',
+            color: 'green',
+            title: 'NFT Green',
+            avatar: '',
+            status: 'done',
+            priority: 1,
+            progress: 15,
+            description: 'Description here.',
+            timestamp: '2023-01-23T09:30:00+0000'
+        },
+    ]
+
+    const uniqueCategories = [
+        ...new Set(tickets?.map(({category}) => category))
+    ]
+
     return (
         <div className="dashboard">
             <h1>My Projects</h1>
 
             <div className="dashboard__ticket-container">
-                <TicketCard/>
+                {tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+                    <div key={categoryIndex}>
+                        <h3>{uniqueCategory}</h3>
+                        {tickets.filter(ticket => ticket.category === uniqueCategory)
+                            .map((filteredTicket, ticketindex) => (
+                                <TicketCard
+                                    id={ticketindex}
+                                    color={filteredTicket.color}
+                                    ticket={filteredTicket}
+                                />
+                            ))
+                        }
+                    </div>
+                ))}
             </div>
         </div>
     )
